1247b079e37b022cb9f3d4d879cce49651fb45e2
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-09-24 19:45:52 -0400

    Added initial state files

---
+++ controllers/controllers.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var router = express.Router();
+
+//var model = require("../models/model.js");
+
+router.get("/", function(req, res){
+    // model.all(function(data){
+    //     res.render("index",data);
+    // });
+});
---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/controller.js");
+app.use(routes);
+
+app.listen(PORT, function(){
+    console.log("Listening on PORT " + PORT);
+})
