2745d4f97683a984abddebcbdb7de14a2625a556
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-09-25 00:18:42 -0400

    Added fortune api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -62,4 +62,63 @@ module.exports = function (app) {
     });
   });
 
+  // ***************** FORTUNE ******************** //
+  // Just lists fortune info without full user details
+  app.get("/api/fortunes", function (req, res) {
+    db.Fortune.findAll({}).then(function (data) {
+      res.json(data);
+    });
+  });
+
+  // One fortune and its users (both sent and received)
+  app.get("/api/fortunes/:id", function (req, res) {
+    db.Fortune.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: {
+        all: true
+      }
+    }).then(function(data){
+      res.json(data);
+    });
+  });
+
+  // One fortune and the user who sent it
+  app.get("/api/fortunes/:id/sent", function (req, res) {
+    db.Fortune.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: {
+        model: db.User,
+        as: 'fromUser'
+      }
+    }).then(function(data){
+      res.json(data);
+    });
+  });
+
+  // One fortune and the user who received it
+  app.get("/api/fortunes/:id/received", function (req, res) {
+    db.Fortune.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: {
+        model: db.User,
+        as: 'toUser'
+      }
+    }).then(function(data){
+      res.json(data);
+    });
+  });
+
+  // Create a new fortune
+  app.post("/api/fortunes", function (req, res) {
+    db.Fortune.create(req.body).then(function (data) {
+      res.json(data);
+    });
+  });
+
 };
