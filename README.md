95e37ff53f478e87039a5fcd8094f57c2a80a4a1
Evan Simon Ross <evan.simon.ross@gmail.com>
2018-09-29 14:04:31 -0400

    Fixed seeds, added credits to readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# team-2-project-2
+# Fortune Cookie
+
+## Credits
+Fortune cookie icon from [Flaticon](https://www.flaticon.com/free-icon/fortune-cookie_284776).
+Fortune cookie banner [Wicked Wonderful Creative](http://wickedwonderfulcreative.com/wp-content/uploads/2015/04/gfc-banner.png).
--- db/seeds.sql
+++ db/seeds.sql
@@ -9,14 +9,14 @@ INSERT INTO
     ('Sandy', '@Sandy_Yeung', 'Slack', NOW(), NOW());
 
 INSERT INTO 
-    fortunes(text, isRead, createdAt, updatedAt, fromUserID)
+    fortunes(text, isRead, createdAt, updatedAt, fromUserID, toUserID)
     VALUES
-    ('Fortune Not Found: Abort, Retry, Ignore?', false, NOW(), NOW(), 1),
-    ('About time I got out of that cookie', false, NOW(), NOW(), 2),
-    ('The early bird gets the worm, but the second mouse gets the cheese', false, NOW(), NOW(), 3),
-    ('Be on alert to recognize your prime at whatever time of your life it may occur', false, NOW(), NOW(), 4),
-    ('Your road to glory will be rocky, but fulfilling', false, NOW(), NOW(), 2),
-    ('Courage is not simply one of the virtues, but the form of every virtue at the testing point', false, NOW(), NOW(), 1);
+    ('Fortune Not Found: Abort, Retry, Ignore?', false, NOW(), NOW(), 1, 2),
+    ('About time I got out of that cookie', false, NOW(), NOW(), 2, 3),
+    ('The early bird gets the worm, but the second mouse gets the cheese', false, NOW(), NOW(), 3, 4),
+    ('Be on alert to recognize your prime at whatever time of your life it may occur', false, NOW(), NOW(), 4, 1),
+    ('Your road to glory will be rocky, but fulfilling', false, NOW(), NOW(), 2, 4),
+    ('Courage is not simply one of the virtues, but the form of every virtue at the testing point', false, NOW(), NOW(), 1, 3);
 
 
 
