509ba9485ee416475b91c05605c08da9d2aa4b4e
JMac <julia@defmethod.com>
2018-09-29 12:05:57 -0400

    Config File

--- config/config.js
+++ config/config.js
@@ -18,11 +18,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_BASE,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    use_env_variable: process.env.JAWSDB_URL,
     dialect: "mysql"
   }
 }
